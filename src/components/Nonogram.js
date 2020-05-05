bf95e706dad24f566834188a1871db9f358aeb36
jacob-miller-hq <jacob.miller.hq@gmail.com>
2020-05-05 12:51:46 -0500

    add clickable grid

--- src/components/Nonogram.js
+++ src/components/Nonogram.js
@@ -1,38 +1,87 @@
 import React, { useState, useRef, useEffect } from 'react'
 
-const DEFAULT_PALETTE = [
-  '#ffffff',
-  '#00003f',
-  '#ff0000',
-  '#00ff7f',
-  '#ffdf00',
-  '#3f00ff',
-  '#804d20',
-  '#ff7fff',
-  '#000000',
-  '#000000',
-  '#000000',
-  '#000000',
-  '#000000',
-  '#000000',
-  '#000000',
-  '#000000',
-]
-
 function Nonogram(props) {
-  const canvas = useRef(null)
+  const canvasRef = useRef(null)
+
+  const [gridSize, setGridSize] = useState(10)
+  const [grid, setGrid] = useState([])
 
   useEffect(() => {
-    const cvs = canvas.current
-    console.log(cvs)
+    setGrid(Array(gridSize * gridSize).fill(0))
+  }, [gridSize])
+
+  useEffect(() => {
+    const cvs = canvasRef.current
     const ctx = cvs.getContext('2d')
+    const w = cvs.width
+    const h = cvs.height
+
+    // clear canvas
     ctx.fillStyle = '#dddddd'
-    ctx.fillRect(0, 0, cvs.width, cvs.height)
-  })
+    ctx.fillRect(0, 0, w, h)
+
+    let colW = (w-1) / gridSize
+    let rowH = (h-1) / gridSize
+    // fill grid
+    grid.forEach((cell, i) => {
+      if (cell !== 0) {
+        let r = Math.floor(i / gridSize)
+        let c = i % gridSize
+        let x = c * colW
+        let y = r * rowH
+
+        ctx.fillStyle = "#11dd11"
+        ctx.fillRect(x, y, colW, rowH)
+      }
+    })
+
+    // outline grid
+    ctx.strokeStyle = '#000000'
+    ctx.beginPath()
+    ctx.moveTo(0, h-1)
+    ctx.lineTo(w-1, h-1)
+    ctx.lineTo(w-1, 0)
+    for(let i = 0; i < gridSize; i++) {
+      let y = i * rowH
+      let x = i * colW
+      ctx.moveTo(0, y)
+      ctx.lineTo(w-1, y)
+      ctx.moveTo(x, 0)
+      ctx.lineTo(x, h-1)
+    }
+    ctx.stroke()
+
+    
+  }, [grid, gridSize])
+
+  const handleClick = e => {
+    const pos = getCursorPos(canvasRef.current, e)
+    const i = posToGridIdx(pos)
+    setGrid([
+      ...grid.slice(0, i),
+      !grid[i],
+      ...grid.slice(i+1)
+    ])
+  }
+
+  const posToGridIdx = (pos) => {
+    const w = canvasRef.current.width
+    const h = canvasRef.current.height
+    const r = Math.floor(pos.y / h * gridSize)
+    const c = Math.floor(pos.x / w * gridSize)
+    return r * gridSize + c
+  }
 
   return (
-    <canvas ref={canvas}/>
+    <canvas ref={canvasRef} onClick={handleClick}/>
   )
+}
+
+function getCursorPos(cvs, e) {
+  const rect = cvs.getBoundingClientRect()
+  const x = Math.floor(e.clientX - rect.left)
+  const y = Math.floor(e.clientY - rect.top)
+  return {x, y}
 }
 
 export default Nonogram
