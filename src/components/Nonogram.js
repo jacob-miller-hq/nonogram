155f9adc2b9b695bc268992de0a4fde751424ac2
jacob-miller-hq <jacob.miller.hq@gmail.com>
2020-05-05 19:07:13 -0500

    format grid

--- src/components/Nonogram.js
+++ src/components/Nonogram.js
@@ -1,15 +1,24 @@
 import React, { useState, useRef, useEffect } from 'react'
 
+function getCursorPos(cvs, e) {
+  const rect = cvs.getBoundingClientRect()
+  const x = Math.floor(e.clientX - rect.left)
+  const y = Math.floor(e.clientY - rect.top)
+  return {x, y}
+}
+
 function Nonogram(props) {
   const {puzzleData} = props
+  const {rows, cols} = puzzleData ? puzzleData : {rows: 10, cols: 10}
 
   const canvasRef = useRef(null)
 
   const [grid, setGrid] = useState([])
+  const [gridRect, setGridRect] = useState({left:0, top:0, right:0, bottom:0})
 
   useEffect(() => {
-    setGrid(Array(puzzleData.rows * puzzleData.cols).fill(0))
-  }, [puzzleData.rows, puzzleData.cols])
+    setGrid(Array(rows * cols).fill(0))
+  }, [rows, cols])
 
   useEffect(() => {
     const cvs = canvasRef.current
@@ -17,49 +26,60 @@ function Nonogram(props) {
     const w = cvs.width
     const h = cvs.height
 
+    // TODO: this probably needs some work
+    const margin = 5
+    const cellSize = Math.min((w - 1 - 2*margin) / cols, (h - 1 - 2*margin) / rows)
+    const gridRight = (w - 1) - margin
+    const gridBottom = (h - 1) - margin
+    const gridLeft = gridRight - (cellSize * cols)
+    const gridTop = gridBottom - (cellSize * rows)
+    setGridRect({
+      left: gridLeft,
+      top: gridTop,
+      right: gridRight,
+      bottom: gridBottom
+    })
+
     // clear canvas
     ctx.fillStyle = '#dddddd'
     ctx.fillRect(0, 0, w, h)
 
-    let colW = (w-1) / puzzleData.cols
-    let rowH = (h-1) / puzzleData.rows
     // fill grid
     grid.forEach((cell, i) => {
       if (cell !== 0) {
-        let r = Math.floor(i / puzzleData.cols)
-        let c = i % puzzleData.cols
-        let x = c * colW
-        let y = r * rowH
+        let r = Math.floor(i / cols)
+        let c = i % cols
+        let x = c * cellSize
+        let y = r * cellSize
 
         ctx.fillStyle = "#11dd11"
-        ctx.fillRect(x, y, colW, rowH)
+        ctx.fillRect(gridLeft + x, gridTop + y, cellSize, cellSize)
       }
     })
 
     // outline grid
     ctx.strokeStyle = '#000000'
     ctx.beginPath()
-    ctx.moveTo(0, h-1)
-    ctx.lineTo(w-1, h-1)
-    ctx.lineTo(w-1, 0)
-    for(let i = 0; i < puzzleData.rows; i++) {
-      let y = i * rowH
-      ctx.moveTo(0, y)
-      ctx.lineTo(w-1, y)
+    ctx.moveTo(gridLeft, gridBottom)
+    ctx.lineTo(gridRight, gridBottom)
+    ctx.lineTo(gridRight, gridTop)
+    for(let i = 0; i < cols; i++) {
+      let x = gridLeft + i * cellSize
+      ctx.moveTo(x, gridTop)
+      ctx.lineTo(x, gridBottom)
     }
-    for(let i = 0; i < puzzleData.cols; i++) {
-      let x = i * colW
-      ctx.moveTo(x, 0)
-      ctx.lineTo(x, h-1)
+    for(let i = 0; i < rows; i++) {
+      let y = gridTop + i * cellSize
+      ctx.moveTo(gridLeft, y)
+      ctx.lineTo(gridRight, y)
     }
     ctx.stroke()
-
     
-  }, [grid, puzzleData])
+  }, [grid, rows, cols])
 
   const handleClick = e => {
     const pos = getCursorPos(canvasRef.current, e)
-    const i = posToGridIdx(pos)
+    const i = posToGridIdx(pos, gridRect)
     setGrid([
       ...grid.slice(0, i),
       !grid[i],
@@ -67,24 +87,16 @@ function Nonogram(props) {
     ])
   }
 
-  const posToGridIdx = (pos) => {
-    const w = canvasRef.current.width
-    const h = canvasRef.current.height
-    const r = Math.floor(pos.y / h * puzzleData.rows)
-    const c = Math.floor(pos.x / w * puzzleData.cols)
-    return r * puzzleData.cols + c
+  const posToGridIdx = (pos, gridRect) => {
+    const {left, top, right, bottom} = gridRect
+    const r = Math.floor((pos.y - top) / (bottom - top) * rows)
+    const c = Math.floor((pos.x - left) / (right - left) * cols)
+    return r * cols + c
   }
 
   return (
     <canvas ref={canvasRef} onClick={handleClick}/>
   )
-}
-
-function getCursorPos(cvs, e) {
-  const rect = cvs.getBoundingClientRect()
-  const x = Math.floor(e.clientX - rect.left)
-  const y = Math.floor(e.clientY - rect.top)
-  return {x, y}
 }
 
 export default Nonogram
